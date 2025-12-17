d6c91deb367aa56adb9c6fec5c455fc7770ff52d
Utkarsh Singh <utkarzz1705@gmail.com>
2025-12-17 22:20:36 +0530

    changed baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"http://localhost:5000/api"
+  baseURL:"hhttps://game-store-project-backend-1.onrender.com/api/games"
 });
 
 export const fetchGames = ()=>
